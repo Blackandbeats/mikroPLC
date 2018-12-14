5e776688759e8308057e050ed2c8d7386c519e41
Willians Briceño <williambriceo567@gmail.com>
2018-12-14 09:54:33 -0400

    Change in formatting

--- mikroPLC-Firmware.c
+++ mikroPLC-Firmware.c
@@ -1,36 +1,45 @@
+
 #include <mikroPLC.c>
 
-void main(){
-   set_tris_a(0x00);
-   set_tris_d(0xFF);
+void main()
+{
+   set_tris_a (0x00) ;
+   set_tris_d (0xFF) ;
    
-   output_a(0x00);
+   output_a (0x00) ;
    
-   disable_interrupts(GLOBAL);
+   disable_interrupts (GLOBAL) ;
    
-   lcd_init();
+   lcd_init () ;
    
-   usb_init();
+   usb_init () ;
    
-   init_ext_eeprom();
+   init_ext_eeprom () ;
    
    int8 plc_menu_option = 0;
    
-   show_menu_option(plc_menu_option);
+   show_menu_option (plc_menu_option) ;
    
-   while(1){
+   while (1)
+   {
       
-      if(input(UP) && (plc_menu_option > 0)){
+      if (input (UP) && (plc_menu_option > 0) )
+      {
          plc_menu_option--;
-         show_menu_option(plc_menu_option);
+         show_menu_option (plc_menu_option) ;
       }
-      else if(input(DOWN) && (plc_menu_option < ( NUMBER_OF_MENU_OPTIONS - 1 ))){
+
+      else if (input (DOWN) && (plc_menu_option < (NUMBER_OF_MENU_OPTIONS - 1 )))
+      {
          plc_menu_option++;
-         show_menu_option(plc_menu_option);
+         show_menu_option (plc_menu_option) ;
       }
-      else if(input(ENTER)){
-         enter_menu_option(plc_menu_option);
-         show_menu_option(plc_menu_option);
-      }  
+
+      else if (input (ENTER))
+      {
+         enter_menu_option (plc_menu_option) ;
+         show_menu_option (plc_menu_option) ;
+      }
    }
 }
+
